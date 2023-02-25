14005828be675f1df296a1c715caf2502b445a74
Egor Volvachev <axe.onegaimy@gmail.com>
2023-02-25 19:14:24 +0300

    feat: add settings for `useUntilDestroy` function

--- projects/angularuse/src/lib/core/use-until-destroy/index.ts
+++ projects/angularuse/src/lib/core/use-until-destroy/index.ts
@@ -1,9 +1,9 @@
 import { MonoTypeOperatorFunction } from 'rxjs';
-import { ChangeDetectorRef, inject, InjectionToken, ViewRef } from '@angular/core';
+import { ChangeDetectorRef, inject, InjectOptions, InjectionToken, ViewRef } from '@angular/core';
 import { untilDestroy } from './internal';
 
-export function useUntilDestroy(): <T>() => MonoTypeOperatorFunction<T> {
-  const viewRef = inject(ChangeDetectorRef) as ViewRef;
+export function useUntilDestroy(options?: InjectOptions): <T>() => MonoTypeOperatorFunction<T> {
+  const viewRef = (options ? inject(ChangeDetectorRef, options) : inject(ChangeDetectorRef)) as ViewRef;
 
   return <T>(): MonoTypeOperatorFunction<T> => untilDestroy<T>(viewRef);
 }
